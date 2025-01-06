118af412b019539169fcb61ba9921ed1efce186a
raka zeniusa <tugasrakazeniusa@gmail.com>
2025-01-06 20:51:38 +0700

    profil page

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -19,6 +19,7 @@ import 'package:provider/provider.dart';
 import 'package:another_flushbar/flushbar.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'package:random_string/random_string.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
